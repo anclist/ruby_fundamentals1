353cbd127d8a943bd875e682326126af15a75338
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-18 14:41:32 -0400

    How gets.chomp works and getting an input and converting it to an integrer or string

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,11 @@
 puts "What is your name?"
 
-user_name = gets
+user_name = gets.chomp.to_s
 
 puts "Hello, #{user_name}"
+
+puts "How old are you?"
+
+age = gets.to_i
+
+puts "#{user_name}, you are #{age} years old."
