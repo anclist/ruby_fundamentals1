4f82c0708f39ad6b39bcf5bfef773307a424cd42
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-18 17:46:49 -0400

    exercise 5.2, give energy level to runner

--- exercise5.rb
+++ exercise5.rb
@@ -2,8 +2,13 @@
 
 walk = "walk"
 run = "run"
+walk_distance = 1
+run_distance = 5
 go_home = "go home"
 distance = 0
+energy = 100
+walk_energy = 15
+run_energy = 10
 
 #Will keep running as long as the distance has a positive number
 while distance >=0
@@ -14,10 +19,14 @@ puts "Would you like to walk, run or go home?"
 
   if answer == walk
 #Adds 1 to the distance, everytime the answer is equal to walk
-    puts "Distance from home is #{distance = distance + 1}km."
+    puts "Distance from home is #{distance += walk_distance}km. Your energy level is #{energy += walk_energy}"
   elsif answer == run
+    if energy > 0
 #Adds 5 to the distance, everytime the answer is equal to walk
-    puts "Distance from home is #{distance = distance + 5}km."
+    puts "Distance from home is #{distance += run_distance}km. Your energy level is #{energy -= run_energy}"
+    else
+    puts "You don't have enough energy to run."
+    end
   elsif answer == go_home
     puts "You're home now, get a shower!"
     distance = -1
