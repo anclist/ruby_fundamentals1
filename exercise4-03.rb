e7b18c6a5e2a91252f3f932d3baf3b190bb0415d
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-18 15:32:57 -0400

    Comparing two strings

---
+++ exercise4-03.rb
@@ -0,0 +1,13 @@
+my_name = "Tony"
+
+puts  "What's your name?"
+
+user_name = gets.chomp.to_s
+
+result = user_name <=> my_name
+
+if result == 0
+  puts "Great! My name is also #{my_name}."
+else
+  puts "Sorry #{user_name}, my name is #{my_name}!"
+end
