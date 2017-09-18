dad5f9d8174f426da13a492cb2f7b94eda59423b
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-18 17:10:49 -0400

    give the user the option to go home

--- exercise5.rb
+++ exercise5.rb
@@ -2,6 +2,7 @@
 
 walk = "walk"
 run = "run"
+go_home = "go home"
 distance = 0
 
 #Will keep running as long as the distance has a positive number
@@ -17,8 +18,11 @@ puts "Would you like to walk, run or go home?"
   elsif answer == run
 #Adds 5 to the distance, everytime the answer is equal to walk
     puts "Distance from home is #{distance = distance + 5}km."
+  elsif answer == go_home
+    puts "You're home now, get a shower!"
+    distance = -1
 #Makes walk or run the only possible answers
-  elsif answer != walk || answer != run
-    puts "That's not the right answer"
+  elsif answer != walk || answer != run || answer != go_home
+    puts "That's not the right answer, the options are walk, run or go home"
   end
 end
