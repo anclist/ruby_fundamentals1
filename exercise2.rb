3ee9afcf5d4c1483213f5ec365adaca91bc2e253
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-18 14:07:19 -0400

    using string interpolation

--- exercise2.rb
+++ exercise2.rb
@@ -3,5 +3,8 @@ tip_percentage = 0.18
 
 puts "If you tip 18% on a $55 bill, your tip value is #{meal_value * tip_percentage} and your total bill is #{meal_value + (meal_value * tip_percentage)}"
 
-
+#TO CONVERT AN INTEGRER INTO A STRING ADD .to_s
 puts "I am " + 34.to_s + " years old"
+
+#USING STRING INTERPOLATION
+puts "The result of multiplying 45628 by 7839 is #{45628 * 7839}"
