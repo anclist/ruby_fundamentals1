b72b0d081fca9775bdbcfbe66856749edc540e0f
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-18 13:55:12 -0400

    calcualting a good tip

---
+++ exercise2.rb
@@ -0,0 +1,4 @@
+meal_value = 55
+tip_percentage = 0.18
+
+puts "If you tip 18% on a $55 bill,  your tip value is #{meal_value * tip_percentage} and your total bill is #{meal_value + (meal_value * tip_percentage)}"
